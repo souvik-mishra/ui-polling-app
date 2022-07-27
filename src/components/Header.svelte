b7ff59b74c426107278fc2595c4266b9ad2ed780
Souvik Misra <70270608+souvik-mishra@users.noreply.github.com>
2022-07-27 08:05:09 +0530

    Added header and  footer components

---
+++ src/components/Header.svelte
@@ -0,0 +1,23 @@
+<script></script>
+
+<header>
+    <h1>
+        <img src="favicon.png" alt="Favicon" />
+    </h1>
+</header>
+
+<style>
+    header {
+        background-color: #f7f7f7;
+        padding: 20px;
+    }
+
+    header > h1 {
+        text-align: center;
+        margin: 0;
+    }
+
+    header > h1 > img {
+        max-width: 180px;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Header from '../components/Header.svelte'
+    import Footer from '../components/Footer.svelte'
+</script>
+
+<Header/>
+<main>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi laborum sint necessitatibus dignissimos omnis nobis assumenda ratione id! Aspernatur, vitae. Mollitia, animi hic! Deleniti earum reiciendis, itaque nam dicta culpa!</p>
+</main>
+<Footer/>
+
+<style>
+    main {
+        max-width: 960px;
+        margin: 40px auto;
+    }
+</style>
